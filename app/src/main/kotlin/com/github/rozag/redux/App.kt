1d5ddc3eaea5c422351651e4ac725b4263acfc71
Alexey Mileev <alemileev@gmail.com>
2017-11-05 00:56:07 +0300

    Initial commit

---
+++ app/src/main/kotlin/com/github/rozag/redux/App.kt
@@ -0,0 +1,12 @@
+package com.github.rozag.redux
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // Do your stuff here (e.g. initialize libraries)
+    }
+
+}
